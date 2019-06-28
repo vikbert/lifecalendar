24217ac1d08ba81f631aa16f0496c315653284e0
Xun Zhou <xun.zhou@lidl.com>
2019-06-28 21:30:11 +0200

    add input to pick the birthday

--- src/components/register/RegisterForm.js
+++ src/components/register/RegisterForm.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import {Container, TextField} from "@material-ui/core";
+import {Container} from "@material-ui/core";
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
 
-const RegisterForm = () => (
-    <Container maxWidth={'sm'}>
-        <TextField
-            label='Nick Name'
-            margin='normal'
-            variant='outlined'
-            name='nickname'
-            fullWidth
-            required
-        />
-    </Container>
-);
+const RegisterForm = () => {
+    // The first commit of Material-UI
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+
+    function handleDateChange(date) {
+        setSelectedDate(date);
+        console.log(date);
+    }
+
+    return (
+        <Container maxWidth={'sm'}>
+            <DatePicker
+                selected={selectedDate}
+                onChange={handleDateChange}
+            />
+        </Container>
+    );
+};
 
 export default RegisterForm;
