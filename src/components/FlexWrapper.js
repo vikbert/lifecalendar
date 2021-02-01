32642ab13ce351fa8eddd6a5828dbda79d4bb4d1
Xun Zhou <segentor@gmail.com>
2021-02-01 18:37:45 +0100

    formatting

--- src/components/FlexWrapper.js
+++ src/components/FlexWrapper.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types'
-import Grid from '@material-ui/core/Grid'
+import PropTypes from 'prop-types';
 import Box from '@material-ui/core/Box';
 
-import DayBox from './DayBox'
+import DayBox from './DayBox';
 
-const FlexWrapper = props => {
+const FlexWrapper = (props) => {
   const days = props.days;
   return (
-    <div style={{width: '100%'}}>
-      <Box display={'flex'} flexWrap={'wrap'} alignContent={'flex-start'} p={1} m={1}>
-          {days.map((day, index) => <DayBox key={index}></DayBox>)}
+    <div style={{ width: '100%' }}>
+      <Box
+        display={'flex'}
+        flexWrap={'wrap'}
+        alignContent={'flex-start'}
+        p={1}
+        m={1}
+      >
+        {days.map((day, index) => (
+          <DayBox key={index}></DayBox>
+        ))}
       </Box>
     </div>
   );
 };
 
 FlexWrapper.propTypes = {
-  days: PropTypes.array.isRequired
+  days: PropTypes.array.isRequired,
 };
 
 export default FlexWrapper;
