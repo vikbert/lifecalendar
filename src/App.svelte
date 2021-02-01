bdafcf4e7b03a458b48040fbe15d88ab69315e93
Xun Zhou <segentor@gmail.com>
2021-02-01 22:57:35 +0100

    init template and rollup config

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  let src = './logo.png';
+</script>
+
+<header class="header">
+  <nav class="topbar space-between">
+    <div class="logo">
+      <img {src} alt="logo" class="logo"/>
+    </div>
+    <button class="is-cleared">Open dialog</button>
+  </nav>
+</header>
+<main class="main">
+
+</main>
+
+<style>
+  .topbar {
+    background-color: white;
+  }
+  .logo {
+    height: 40px;
+    margin-left: 2px;
+  }
+  .main {
+    padding: 8px;
+  }
+</style>
